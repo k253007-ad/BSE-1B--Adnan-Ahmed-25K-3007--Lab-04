0f68f570086a8ff17239d3a2c180102de2f0c268
Adnan_Ahmed_25K-3007 <k253007@nu.edu.pk>
2025-09-11 22:49:46 -0700

    Update Lab4-Pizza-Order.c

--- Lab4-Pizza-Order.c
+++ Lab4-Pizza-Order.c
@@ -3,20 +3,19 @@
 int main() {
     // Pizza Man
     int pizza;
-    printf("Welcome to Karachi Pizza");
+    printf("Welcome to Pizza Hut");
     printf("\nWhat would you like to have");
     printf("\n1.Small \n2.Medium \n3.Large");
     printf("\nEnter Size : ");
     scanf("%d",&pizza);
-    if(pizza == 1){
-        printf("\n That will be Rs:350");
-    } else if(pizza == 2){
-        printf("\n That will be Rs:500");
+    if( pizza == 1 ){
+        printf("\n That will be $8");
+    } else if( pizza == 2 ){
+        printf("\n That will be $15");
     } else if ( pizza == 3 ) {
-        printf("\n That will be Rs:850");
+        printf("\n That will be $21");
     } else {
-        printf("\n Enter Correct Number");
+        printf("\n Invalid Pizza");
     }
-
     return 0;
 }
